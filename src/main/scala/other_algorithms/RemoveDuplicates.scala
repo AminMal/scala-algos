482e20322b6c7be0a5d0b93726c4fdb24c4d994b
AminMal <amin.malekloo@yahoo.com>
2020-11-19 19:10:00 +0330

    remove duplicates algo, functinal approach added

---
+++ src/main/scala/other_algorithms/RemoveDuplicates.scala
@@ -0,0 +1,46 @@
+package other_algorithms
+
+object RemoveDuplicates extends App {
+
+  val source: List[Int] = List(2, 2, 4, 5, 7, 7, 8)
+  // Remove the duplicated elements and return distinnct ones
+  // output => [4, 5, 8]
+  /**
+   * functional approach
+   */
+  // first i need an exists(elem) function on list which does not suck!
+  implicit class ListOps[T](value: List[T]) {
+    def has(elem: T): Boolean = {
+      def iterate(index: Int = 0): Boolean = {
+        if (index >= value.length) false
+        else {
+          if (value(index) == elem) true
+          else iterate(index + 1)
+        }
+      }
+      iterate()
+    }
+  }
+
+  def removeDuplicates(input: List[Int]): List[Int] = {
+    def iterator(
+                index: Int = 0,
+                whole: List[Int] = input,
+                extras: List[Int] = List(),
+                acc: List[Int] = List()
+                ): List[Int] = {
+      if (whole.isEmpty) acc
+      else {
+        val currentVal = whole.head
+        if (extras.has(currentVal) || whole.tail.has(currentVal)) iterator(
+          index + 1, whole.tail, extras :+ currentVal, acc)
+        
+        else iterator(index + 1, whole.tail, extras, acc :+ currentVal)
+      }
+    }
+    iterator()
+  }
+  
+  val removed = removeDuplicates(source)
+  println(removed)
+}
