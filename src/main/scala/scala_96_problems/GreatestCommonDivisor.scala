6556fcb9f8eca6cc439a572d5fe537016d285607
AminMal <amin.malekloo@yahoo.com>
2020-12-01 02:18:26 +0330

    main function added to gcd for test

--- src/main/scala/scala_96_problems/GreatestCommonDivisor.scala
+++ src/main/scala/scala_96_problems/GreatestCommonDivisor.scala
@@ -2,7 +2,11 @@ package scala_96_problems
 
 object GreatestCommonDivisor {
 
-  def gcd(a: Int, b: Int): Int = 
+  def gcd(a: Int, b: Int): Int =
     if b == 0 then a else gcd(b, a % b)
-  
+
+  def main(args: Array[String]): Unit = {
+    println(gcd(4, 7))
+    println(gcd(4, 8))
+  }
 }
